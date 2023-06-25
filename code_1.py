8646e3fbc37a8cc12f9f856527a16b5a8cb00772
Matheus Oliveira <112136868+matheus-oliveira-venancio@users.noreply.github.com>
2023-06-25 11:35:50 -0300

    Add files via upload

---
+++ code_1.py
@@ -0,0 +1,5 @@
+print ("welcome to robo barista cafe",)
+name = input ("what is your name")
+print ("hello", name, "what do you want to order")
+menu = input ("we've java coffee, coffee with milk and normal coffee what do you want to order")
+print ("okay mr", name, "we'll have that", menu, "ready for you in a comment") 
